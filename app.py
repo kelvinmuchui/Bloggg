a8954835084af979825a80cd1c777901e23e33a4
kelvinmuchui <muchuikelvin423@gmail.com>
2016-05-18 10:25:43 +0300

    authentification 1

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template, redirect,url_for,request
+from flask import Flask, render_template, redirect,url_for,request, session, flash
+from functools import wraps
 
 app = Flask(__name__)
 
+app.secret_key = "never guess "
+
+#login required decorator
+def  login_required(f):
+    if 'logged_in' in session:
+        return f(*args, **kwargs)
+    else:
+        flash('you need to login first.')
+        return redirect(url_for('login'))
+return wrap
+
 @app.route('/')
 def home():
-    return "Hello, world!"
+   #return "Hello, world!"
+   return render_template('index.html')
 
 
 @app.route('/welcome')
@@ -19,8 +32,20 @@ def login():
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
-            return redirect(url_for('home'))
+        	session['logged_in']= True
+        flash('You have logged in ')
+        return redirect(url_for('home'))
     return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+
+        flash('You have logged out')
+	return redirect(url_for('welcome'))
+
+
 
 if __name__ == '__main__':
     app.run(debug = True) 
